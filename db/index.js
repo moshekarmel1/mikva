a8f73f1423f8d98bac74d9454222769eb260d01f
Karmel, Moshe <moshekarmel@quickenloans.com>
2021-06-28 16:30:47 -0400

    ssl

--- db/index.js
+++ db/index.js
@@ -1,7 +1,9 @@
 const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: false,
+  ssl: {
+    rejectUnauthorized: false
+  },
   idleTimeoutMillis: 0,
   connectionTimeoutMillis: 0,
 });
