d4f70b043fada1679174d73afbee7a7f92dd50a1
Karmel, Moshe <moshekarmel@quickenloans.com>
2023-11-22 13:57:21 -0500

    cleaning up a bunch of :poop: 2

--- db/index.js
+++ db/index.js
@@ -1,10 +1,10 @@
 const { Pool } = require('pg');
 const pool = new Pool({
-  connectionString: 'postgres://postgres:1234@localhost:5432/postgres' || process.env.DATABASE_URL,
-  ssl: false,
-  // ssl: {
-  //   rejectUnauthorized: false
-  // },
+  connectionString: process.env.DATABASE_URL || 'postgres://postgres:1234@localhost:5432/postgres',
+  // ssl: false,
+  ssl: {
+    rejectUnauthorized: false
+  },
   idleTimeoutMillis: 0,
   connectionTimeoutMillis: 0,
 });
